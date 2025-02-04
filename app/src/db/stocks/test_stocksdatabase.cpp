89bb39058f01928e9f8452355f861b6f991e67f9
Gris87 <Gris87@yandex.ru>
2025-02-04 20:38:36 +0300

    More tests

--- app/src/db/stocks/test_stocksdatabase.cpp
+++ app/src/db/stocks/test_stocksdatabase.cpp
@@ -181,6 +181,209 @@ TEST_F(Test_StocksDatabase, Test_readStocksData)
     ASSERT_NEAR(stocks.at(2).data.at(4).value,   100, 0.001f);
 }
 
+TEST_F(Test_StocksDatabase, Test_writeStocksMeta)
+{
+    QList<Stock> stocks;
+
+    Stock stock1;
+    Stock stock2;
+    Stock stock3;
+
+    StockData stockData1;
+    StockData stockData2;
+    StockData stockData3;
+    StockData stockData4;
+
+    stock1.name = "TEST";
+    stock2.name = "MAGA";
+    stock3.name = "HNYA";
+
+    stock1.fullname = "abc";
+    stock2.fullname = "def";
+    stock3.fullname = "aaaa";
+
+    stockData1.timestamp = 100;
+    stockData2.timestamp = 200;
+    stockData3.timestamp = 300;
+    stockData4.timestamp = 400;
+
+    stockData1.value = 0.1f;
+    stockData2.value = 0.2f;
+    stockData3.value = 0.3f;
+    stockData4.value = 0.4f;
+
+    stock1.data << stockData1 << stockData2;
+    stock2.data << stockData2 << stockData3;
+    stock3.data << stockData1 << stockData3 << stockData4;
+    stocks << stock1 << stock2 << stock3;
+
+    database->writeStocksMeta(&stocks);
+    stocks = database->readStocksMeta();
+
+    ASSERT_EQ(stocks.size(),                3);
+    ASSERT_EQ(stocks.at(0).name,            "TEST");
+    ASSERT_EQ(stocks.at(0).fullname,        "abc");
+    ASSERT_EQ(stocks.at(0).data.size(),     0);
+    ASSERT_EQ(stocks.at(0).data.capacity(), 0);
+
+    ASSERT_EQ(stocks.at(1).name,            "MAGA");
+    ASSERT_EQ(stocks.at(1).fullname,        "def");
+    ASSERT_EQ(stocks.at(1).data.size(),     0);
+    ASSERT_EQ(stocks.at(1).data.capacity(), 0);
+
+    ASSERT_EQ(stocks.at(2).name,            "HNYA");
+    ASSERT_EQ(stocks.at(2).fullname,        "aaaa");
+    ASSERT_EQ(stocks.at(2).data.size(),     0);
+    ASSERT_EQ(stocks.at(2).data.capacity(), 0);
+}
+
+TEST_F(Test_StocksDatabase, Test_appendStockData)
+{
+    QList<Stock> stocks = database->readStocksMeta();
+
+    ASSERT_EQ(stocks.size(),                3);
+    ASSERT_EQ(stocks.at(0).name,            "AZAZ0");
+    ASSERT_EQ(stocks.at(0).fullname,        "BLAH 0");
+    ASSERT_EQ(stocks.at(0).data.size(),     0);
+    ASSERT_EQ(stocks.at(0).data.capacity(), 0);
+
+    ASSERT_EQ(stocks.at(1).name,            "AZAZ1");
+    ASSERT_EQ(stocks.at(1).fullname,        "BLAH 1");
+    ASSERT_EQ(stocks.at(1).data.size(),     0);
+    ASSERT_EQ(stocks.at(1).data.capacity(), 0);
+
+    ASSERT_EQ(stocks.at(2).name,            "AZAZ2");
+    ASSERT_EQ(stocks.at(2).fullname,        "BLAH 2");
+    ASSERT_EQ(stocks.at(2).data.size(),     0);
+    ASSERT_EQ(stocks.at(2).data.capacity(), 0);
+
+    database->readStocksData(&stocks);
+
+    ASSERT_EQ(stocks.size(),                     3);
+    ASSERT_EQ(stocks.at(0).name,                 "AZAZ0");
+    ASSERT_EQ(stocks.at(0).fullname,             "BLAH 0");
+    ASSERT_EQ(stocks.at(0).data.size(),          3);
+    ASSERT_EQ(stocks.at(0).data.capacity(),      1443);
+    ASSERT_EQ(stocks.at(0).data.at(0).timestamp, 100);
+    ASSERT_NEAR(stocks.at(0).data.at(0).value,   20, 0.001f);
+    ASSERT_EQ(stocks.at(0).data.at(1).timestamp, 200);
+    ASSERT_NEAR(stocks.at(0).data.at(1).value,   1000, 0.001f);
+    ASSERT_EQ(stocks.at(0).data.at(2).timestamp, 300);
+    ASSERT_NEAR(stocks.at(0).data.at(2).value,   500, 0.001f);
+
+    ASSERT_EQ(stocks.at(1).name,                 "AZAZ1");
+    ASSERT_EQ(stocks.at(1).fullname,             "BLAH 1");
+    ASSERT_EQ(stocks.at(1).data.size(),          6);
+    ASSERT_EQ(stocks.at(1).data.capacity(),      1446);
+    ASSERT_EQ(stocks.at(1).data.at(0).timestamp, 150);
+    ASSERT_NEAR(stocks.at(1).data.at(0).value,   1000.0f, 0.001f);
+    ASSERT_EQ(stocks.at(1).data.at(1).timestamp, 250);
+    ASSERT_NEAR(stocks.at(1).data.at(1).value,   999.85f, 0.001f);
+    ASSERT_EQ(stocks.at(1).data.at(2).timestamp, 350);
+    ASSERT_NEAR(stocks.at(1).data.at(2).value,   1000.35f, 0.001f);
+    ASSERT_EQ(stocks.at(1).data.at(3).timestamp, 450);
+    ASSERT_NEAR(stocks.at(1).data.at(3).value,   875.95f, 0.001f);
+    ASSERT_EQ(stocks.at(1).data.at(4).timestamp, 550);
+    ASSERT_NEAR(stocks.at(1).data.at(4).value,   1550.75f, 0.001f);
+    ASSERT_EQ(stocks.at(1).data.at(5).timestamp, 650);
+    ASSERT_NEAR(stocks.at(1).data.at(5).value,   650.15f, 0.001f);
+
+    ASSERT_EQ(stocks.at(2).name,                 "AZAZ2");
+    ASSERT_EQ(stocks.at(2).fullname,             "BLAH 2");
+    ASSERT_EQ(stocks.at(2).data.size(),          5);
+    ASSERT_EQ(stocks.at(2).data.capacity(),      1445);
+    ASSERT_EQ(stocks.at(2).data.at(0).timestamp, 120);
+    ASSERT_NEAR(stocks.at(2).data.at(0).value,   300, 0.001f);
+    ASSERT_EQ(stocks.at(2).data.at(1).timestamp, 220);
+    ASSERT_NEAR(stocks.at(2).data.at(1).value,   130, 0.001f);
+    ASSERT_EQ(stocks.at(2).data.at(2).timestamp, 320);
+    ASSERT_NEAR(stocks.at(2).data.at(2).value,   450, 0.001f);
+    ASSERT_EQ(stocks.at(2).data.at(3).timestamp, 420);
+    ASSERT_NEAR(stocks.at(2).data.at(3).value,   600, 0.001f);
+    ASSERT_EQ(stocks.at(2).data.at(4).timestamp, 520);
+    ASSERT_NEAR(stocks.at(2).data.at(4).value,   100, 0.001f);
+
+
+
+    StockData stockData1;
+    StockData stockData2;
+    StockData stockData3;
+    StockData stockData4;
+
+    stockData1.timestamp = 100;
+    stockData2.timestamp = 200;
+    stockData3.timestamp = 300;
+    stockData4.timestamp = 400;
+
+    stockData1.value = 0.1f;
+    stockData2.value = 0.2f;
+    stockData3.value = 0.3f;
+    stockData4.value = 0.4f;
+
+    Stock stock;
+
+    stock.name = "AZAZ0";
+    stock.data << stockData1 << stockData2 << stockData3 << stockData4;
+
+    database->appendStockData(&stock);
+
+
+
+    database->readStocksData(&stocks);
+
+    ASSERT_EQ(stocks.size(),                     3);
+    ASSERT_EQ(stocks.at(0).name,                 "AZAZ0");
+    ASSERT_EQ(stocks.at(0).fullname,             "BLAH 0");
+    ASSERT_EQ(stocks.at(0).data.size(),          7);
+    ASSERT_EQ(stocks.at(0).data.capacity(),      1447);
+    ASSERT_EQ(stocks.at(0).data.at(0).timestamp, 100);
+    ASSERT_NEAR(stocks.at(0).data.at(0).value,   20, 0.001f);
+    ASSERT_EQ(stocks.at(0).data.at(1).timestamp, 200);
+    ASSERT_NEAR(stocks.at(0).data.at(1).value,   1000, 0.001f);
+    ASSERT_EQ(stocks.at(0).data.at(2).timestamp, 300);
+    ASSERT_NEAR(stocks.at(0).data.at(2).value,   500, 0.001f);
+    ASSERT_EQ(stocks.at(0).data.at(3).timestamp, 100);
+    ASSERT_NEAR(stocks.at(0).data.at(3).value,   0.1f, 0.001f);
+    ASSERT_EQ(stocks.at(0).data.at(4).timestamp, 200);
+    ASSERT_NEAR(stocks.at(0).data.at(4).value,   0.2f, 0.001f);
+    ASSERT_EQ(stocks.at(0).data.at(5).timestamp, 300);
+    ASSERT_NEAR(stocks.at(0).data.at(5).value,   0.3f, 0.001f);
+    ASSERT_EQ(stocks.at(0).data.at(6).timestamp, 400);
+    ASSERT_NEAR(stocks.at(0).data.at(6).value,   0.4f, 0.001f);
+
+    ASSERT_EQ(stocks.at(1).name,                 "AZAZ1");
+    ASSERT_EQ(stocks.at(1).fullname,             "BLAH 1");
+    ASSERT_EQ(stocks.at(1).data.size(),          6);
+    ASSERT_EQ(stocks.at(1).data.capacity(),      1446);
+    ASSERT_EQ(stocks.at(1).data.at(0).timestamp, 150);
+    ASSERT_NEAR(stocks.at(1).data.at(0).value,   1000.0f, 0.001f);
+    ASSERT_EQ(stocks.at(1).data.at(1).timestamp, 250);
+    ASSERT_NEAR(stocks.at(1).data.at(1).value,   999.85f, 0.001f);
+    ASSERT_EQ(stocks.at(1).data.at(2).timestamp, 350);
+    ASSERT_NEAR(stocks.at(1).data.at(2).value,   1000.35f, 0.001f);
+    ASSERT_EQ(stocks.at(1).data.at(3).timestamp, 450);
+    ASSERT_NEAR(stocks.at(1).data.at(3).value,   875.95f, 0.001f);
+    ASSERT_EQ(stocks.at(1).data.at(4).timestamp, 550);
+    ASSERT_NEAR(stocks.at(1).data.at(4).value,   1550.75f, 0.001f);
+    ASSERT_EQ(stocks.at(1).data.at(5).timestamp, 650);
+    ASSERT_NEAR(stocks.at(1).data.at(5).value,   650.15f, 0.001f);
+
+    ASSERT_EQ(stocks.at(2).name,                 "AZAZ2");
+    ASSERT_EQ(stocks.at(2).fullname,             "BLAH 2");
+    ASSERT_EQ(stocks.at(2).data.size(),          5);
+    ASSERT_EQ(stocks.at(2).data.capacity(),      1445);
+    ASSERT_EQ(stocks.at(2).data.at(0).timestamp, 120);
+    ASSERT_NEAR(stocks.at(2).data.at(0).value,   300, 0.001f);
+    ASSERT_EQ(stocks.at(2).data.at(1).timestamp, 220);
+    ASSERT_NEAR(stocks.at(2).data.at(1).value,   130, 0.001f);
+    ASSERT_EQ(stocks.at(2).data.at(2).timestamp, 320);
+    ASSERT_NEAR(stocks.at(2).data.at(2).value,   450, 0.001f);
+    ASSERT_EQ(stocks.at(2).data.at(3).timestamp, 420);
+    ASSERT_NEAR(stocks.at(2).data.at(3).value,   600, 0.001f);
+    ASSERT_EQ(stocks.at(2).data.at(4).timestamp, 520);
+    ASSERT_NEAR(stocks.at(2).data.at(4).value,   100, 0.001f);
+}
+
 TEST_F(Test_StocksDatabase, Test_deleteObsoleteData)
 {
     QList<Stock> stocks = database->readStocksMeta();
