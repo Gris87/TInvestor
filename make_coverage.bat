4c5b78386ca96fdac38a34b167285efd0e46f7f4
Gris87 <Gris87@yandex.ru>
2025-04-25 00:21:00 +0300

    Multiple attemps to calculate coverage on Windows

--- make_coverage.bat
+++ make_coverage.bat
@@ -3,27 +3,41 @@ cd build\Desktop-Debug\test\build
 tests.exe > output.txt 2>&1
 set RESULT_CODE=%ERRORLEVEL%
 
-if %RESULT_CODE% EQU 0 (
-    OpenCppCoverage ^
-        --sources app\src\ ^
-        --excluded_sources test_* ^
-        --excluded_sources *_mock.h ^
-        --excluded_sources messagebox.cpp ^
-        --excluded_sources httpclient.cpp ^
-        --export_type html:..\..\..\CoverageReport ^
-        --export_type cobertura:..\..\..\CoverageReport\cobertura.xml ^
-        -- ^
-        tests.exe
-    set RESULT_CODE=%ERRORLEVEL%
-
-    if not "%1" == "--ci" ..\..\..\CoverageReport\index.html
-) else (
+if %RESULT_CODE% NEQ 0 (
     type output.txt
+    del output.txt
+
+    cd ..\..\..\..
+
     if not "%1" == "--ci" pause
+
+    exit /b %RESULT_CODE%
 )
 
 del output.txt
 
+CALL :try
+set RESULT_CODE=%ERRORLEVEL%
+
+if not "%1" == "--ci" ..\..\..\CoverageReport\index.html
 cd ..\..\..\..
 
 exit /b %RESULT_CODE%
+
+:try
+SET /A tries=5
+
+:loop
+IF %tries% LEQ 0 EXIT /B 1
+
+SET /A tries-=1
+OpenCppCoverage ^
+    --sources app\src\ ^
+    --excluded_sources test_* ^
+    --excluded_sources *_mock.h ^
+    --excluded_sources messagebox.cpp ^
+    --excluded_sources httpclient.cpp ^
+    --export_type html:..\..\..\CoverageReport ^
+    --export_type cobertura:..\..\..\CoverageReport\cobertura.xml ^
+    -- ^
+    tests.exe && (EXIT /B 0) || (GOTO loop)
