52400db459ed222767b9a3424069478f36a085fd
Gris87 <Gris87@yandex.ru>
2025-05-04 13:31:43 +0300

    More tests

--- app/src/storage/instruments/test_instrumentsstorage.cpp
+++ app/src/storage/instruments/test_instrumentsstorage.cpp
@@ -39,3 +39,121 @@ protected:
 TEST_F(Test_InstrumentsStorage, Test_constructor_and_destructor)
 {
 }
+
+TEST_F(Test_InstrumentsStorage, Test_readFromDatabase_and_getInstruments)
+{
+    const InSequence seq;
+
+    Instruments instruments = storage->getInstruments();
+    ASSERT_EQ(instruments.size(), 0);
+
+    InstrumentInfo instrument1;
+    InstrumentInfo instrument2;
+    InstrumentInfo instrument3;
+
+    instrument1.ticker = "BANA";
+    instrument1.name   = "Banana";
+
+    instrument2.ticker = "BODO";
+    instrument2.name   = "BODO NE BODO";
+
+    instrument3.ticker = "ZARA";
+    instrument3.name   = "Zaraza";
+
+    instruments["aaaaa"] = instrument1;
+    instruments["bbbbb"] = instrument2;
+    instruments["ccccc"] = instrument3;
+
+    EXPECT_CALL(*instrumentsDatabaseMock, readInstruments()).WillOnce(Return(instruments));
+
+    storage->readFromDatabase();
+    instruments = storage->getInstruments();
+
+    // clang-format off
+    ASSERT_EQ(instruments.size(),          3);
+    ASSERT_EQ(instruments["aaaaa"].ticker, "BANA");
+    ASSERT_EQ(instruments["aaaaa"].name,   "Banana");
+    ASSERT_EQ(instruments["bbbbb"].ticker, "BODO");
+    ASSERT_EQ(instruments["bbbbb"].name,   "BODO NE BODO");
+    ASSERT_EQ(instruments["ccccc"].ticker, "ZARA");
+    ASSERT_EQ(instruments["ccccc"].name,   "Zaraza");
+    // clang-format on
+}
+
+TEST_F(Test_InstrumentsStorage, Test_getMutex)
+{
+    ASSERT_NE(storage->getMutex(), nullptr);
+}
+
+TEST_F(Test_InstrumentsStorage, Test_mergeInstruments)
+{
+    const InSequence seq;
+
+    Instruments instruments = storage->getInstruments();
+    ASSERT_EQ(instruments.size(), 0);
+
+    InstrumentInfo instrument1;
+    InstrumentInfo instrument2;
+    InstrumentInfo instrument3;
+
+    instrument1.ticker = "BANA";
+    instrument1.name   = "Banana";
+
+    instrument2.ticker = "BODO";
+    instrument2.name   = "BODO NE BODO";
+
+    instrument3.ticker = "ZARA";
+    instrument3.name   = "Zaraza";
+
+    instruments["aaaaa"] = instrument1;
+    instruments["bbbbb"] = instrument2;
+    instruments["ccccc"] = instrument3;
+
+    EXPECT_CALL(*instrumentsDatabaseMock, readInstruments()).WillOnce(Return(instruments));
+
+    storage->readFromDatabase();
+    instruments = storage->getInstruments();
+
+    // clang-format off
+    ASSERT_EQ(instruments.size(),          3);
+    ASSERT_EQ(instruments["aaaaa"].ticker, "BANA");
+    ASSERT_EQ(instruments["aaaaa"].name,   "Banana");
+    ASSERT_EQ(instruments["bbbbb"].ticker, "BODO");
+    ASSERT_EQ(instruments["bbbbb"].name,   "BODO NE BODO");
+    ASSERT_EQ(instruments["ccccc"].ticker, "ZARA");
+    ASSERT_EQ(instruments["ccccc"].name,   "Zaraza");
+    // clang-format on
+
+    Instruments newInstruments;
+
+    InstrumentInfo newInstrument1;
+    InstrumentInfo newInstrument2;
+
+    newInstrument1.ticker = "WADA";
+    newInstrument1.name   = "Awada kedabra";
+
+    newInstrument2.ticker = "NIGA";
+    newInstrument2.name   = "Read my kniga";
+
+    newInstruments["bbbbb"] = newInstrument1;
+    newInstruments["ddddd"] = newInstrument2;
+
+    EXPECT_CALL(*instrumentsDatabaseMock, writeInstruments(Ne(Instruments())));
+
+    storage->mergeInstruments(newInstruments);
+    storage->mergeInstruments(newInstruments);
+
+    instruments = storage->getInstruments();
+
+    // clang-format off
+    ASSERT_EQ(instruments.size(),          4);
+    ASSERT_EQ(instruments["aaaaa"].ticker, "BANA");
+    ASSERT_EQ(instruments["aaaaa"].name,   "Banana");
+    ASSERT_EQ(instruments["bbbbb"].ticker, "WADA");
+    ASSERT_EQ(instruments["bbbbb"].name,   "Awada kedabra");
+    ASSERT_EQ(instruments["ccccc"].ticker, "ZARA");
+    ASSERT_EQ(instruments["ccccc"].name,   "Zaraza");
+    ASSERT_EQ(instruments["ddddd"].ticker, "NIGA");
+    ASSERT_EQ(instruments["ddddd"].name,   "Read my kniga");
+    // clang-format on
+}
