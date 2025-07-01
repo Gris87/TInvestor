800eb323098a197e297bf4615a1598daec9920ec
Gris87 <Gris87@yandex.ru>
2025-07-01 16:27:54 +0300

    Improve test coverage

--- app/src/widgets/logsfilterwidget/test_logsfilterwidget.cpp
+++ app/src/widgets/logsfilterwidget/test_logsfilterwidget.cpp
@@ -26,3 +26,110 @@ protected:
 TEST_F(Test_LogsFilterWidget, Test_constructor_and_destructor)
 {
 }
+
+TEST_F(Test_LogsFilterWidget, Test_on_verboseButton_clicked)
+{
+    logsFilterWidget->ui->debugButton->click();
+    logsFilterWidget->ui->verboseButton->click();
+
+    const LogFilter filter = logsFilterWidget->getFilter();
+
+    // clang-format off
+    ASSERT_EQ(filter.level,                                     LOG_LEVEL_VERBOSE);
+    ASSERT_EQ(filter.ticker,                                    "");
+    ASSERT_EQ(logsFilterWidget->ui->verboseButton->isChecked(), true);
+    ASSERT_EQ(logsFilterWidget->ui->debugButton->isChecked(),   false);
+    ASSERT_EQ(logsFilterWidget->ui->infoButton->isChecked(),    false);
+    ASSERT_EQ(logsFilterWidget->ui->warningButton->isChecked(), false);
+    ASSERT_EQ(logsFilterWidget->ui->errorButton->isChecked(),   false);
+    // clang-format on
+}
+
+TEST_F(Test_LogsFilterWidget, Test_on_debugButton_clicked)
+{
+    logsFilterWidget->ui->debugButton->click();
+
+    const LogFilter filter = logsFilterWidget->getFilter();
+
+    // clang-format off
+    ASSERT_EQ(filter.level,                                     LOG_LEVEL_DEBUG);
+    ASSERT_EQ(filter.ticker,                                    "");
+    ASSERT_EQ(logsFilterWidget->ui->verboseButton->isChecked(), false);
+    ASSERT_EQ(logsFilterWidget->ui->debugButton->isChecked(),   true);
+    ASSERT_EQ(logsFilterWidget->ui->infoButton->isChecked(),    false);
+    ASSERT_EQ(logsFilterWidget->ui->warningButton->isChecked(), false);
+    ASSERT_EQ(logsFilterWidget->ui->errorButton->isChecked(),   false);
+    // clang-format on
+}
+
+TEST_F(Test_LogsFilterWidget, Test_on_infoButton_clicked)
+{
+    logsFilterWidget->ui->infoButton->click();
+
+    const LogFilter filter = logsFilterWidget->getFilter();
+
+    // clang-format off
+    ASSERT_EQ(filter.level,                                     LOG_LEVEL_INFO);
+    ASSERT_EQ(filter.ticker,                                    "");
+    ASSERT_EQ(logsFilterWidget->ui->verboseButton->isChecked(), false);
+    ASSERT_EQ(logsFilterWidget->ui->debugButton->isChecked(),   false);
+    ASSERT_EQ(logsFilterWidget->ui->infoButton->isChecked(),    true);
+    ASSERT_EQ(logsFilterWidget->ui->warningButton->isChecked(), false);
+    ASSERT_EQ(logsFilterWidget->ui->errorButton->isChecked(),   false);
+    // clang-format on
+}
+
+TEST_F(Test_LogsFilterWidget, Test_on_warningButton_clicked)
+{
+    logsFilterWidget->ui->warningButton->click();
+
+    const LogFilter filter = logsFilterWidget->getFilter();
+
+    // clang-format off
+    ASSERT_EQ(filter.level,                                     LOG_LEVEL_WARNING);
+    ASSERT_EQ(filter.ticker,                                    "");
+    ASSERT_EQ(logsFilterWidget->ui->verboseButton->isChecked(), false);
+    ASSERT_EQ(logsFilterWidget->ui->debugButton->isChecked(),   false);
+    ASSERT_EQ(logsFilterWidget->ui->infoButton->isChecked(),    false);
+    ASSERT_EQ(logsFilterWidget->ui->warningButton->isChecked(), true);
+    ASSERT_EQ(logsFilterWidget->ui->errorButton->isChecked(),   false);
+    // clang-format on
+}
+
+TEST_F(Test_LogsFilterWidget, Test_on_errorButton_clicked)
+{
+    logsFilterWidget->ui->errorButton->click();
+
+    const LogFilter filter = logsFilterWidget->getFilter();
+
+    // clang-format off
+    ASSERT_EQ(filter.level,                                     LOG_LEVEL_ERROR);
+    ASSERT_EQ(filter.ticker,                                    "");
+    ASSERT_EQ(logsFilterWidget->ui->verboseButton->isChecked(), false);
+    ASSERT_EQ(logsFilterWidget->ui->debugButton->isChecked(),   false);
+    ASSERT_EQ(logsFilterWidget->ui->infoButton->isChecked(),    false);
+    ASSERT_EQ(logsFilterWidget->ui->warningButton->isChecked(), false);
+    ASSERT_EQ(logsFilterWidget->ui->errorButton->isChecked(),   true);
+    // clang-format on
+}
+
+TEST_F(Test_LogsFilterWidget, Test_on_tickerLineEdit_textChanged_and_filterChangeDelayTimerTicked)
+{
+    logsFilterWidget->ui->tickerLineEdit->setText("aaaaa");
+
+    const LogFilter filter = logsFilterWidget->getFilter();
+
+    // clang-format off
+    ASSERT_EQ(filter.level,                                        LOG_LEVEL_VERBOSE);
+    ASSERT_EQ(filter.ticker,                                       "aaaaa");
+    ASSERT_EQ(logsFilterWidget->filterChangeDelayTimer.interval(), 1000);
+    ASSERT_EQ(logsFilterWidget->filterChangeDelayTimer.isActive(), true);
+    // clang-format on
+
+    logsFilterWidget->filterChangeDelayTimerTicked();
+
+    // clang-format off
+    ASSERT_EQ(logsFilterWidget->filterChangeDelayTimer.interval(), 1000);
+    ASSERT_EQ(logsFilterWidget->filterChangeDelayTimer.isActive(), false);
+    // clang-format on
+}
--- app/src/widgets/tabledelegates/test_instrumentitemdelegate.cpp
+++ app/src/widgets/tabledelegates/test_instrumentitemdelegate.cpp
@@ -1,9 +1,113 @@
 #include "src/widgets/tabledelegates/instrumentitemdelegate.h"
 
+#include <QPainter>
+#include <QTreeView>
 #include <gtest/gtest.h>
 
+#include "src/widgets/tablemodels/logstablemodel/ilogstablemodel_mock.h"
+#include "src/widgets/tablemodels/modelroles.h"
 
 
-TEST(Test_InstrumentItemDelegate, Test_constructor_and_destructor)
+
+using ::testing::InSequence;
+using ::testing::Return;
+using ::testing::StrictMock;
+
+
+
+class Test_InstrumentItemDelegate : public ::testing::Test
 {
+protected:
+    void SetUp() override
+    {
+        treeView           = new QTreeView();
+        logsTableModelMock = new StrictMock<LogsTableModelMock>();
+
+        item = new InstrumentItemDelegate(treeView);
+    }
+
+    void TearDown() override
+    {
+        delete item;
+        delete treeView;
+        delete logsTableModelMock;
+    }
+
+    InstrumentItemDelegate*         item;
+    QTreeView*                      treeView;
+    StrictMock<LogsTableModelMock>* logsTableModelMock;
+};
+
+
+
+TEST_F(Test_InstrumentItemDelegate, Test_constructor_and_destructor)
+{
+}
+
+TEST_F(Test_InstrumentItemDelegate, Test_paint)
+{
+    const InSequence seq;
+
+    Logo logo;
+
+    EXPECT_CALL(*logsTableModelMock, rowCount(QModelIndex())).WillOnce(Return(1));
+    EXPECT_CALL(*logsTableModelMock, columnCount(QModelIndex())).WillOnce(Return(1));
+
+    QPainter             painter;
+    QStyleOptionViewItem option;
+    QModelIndex          index = logsTableModelMock->index(0, 0);
+
+    option.state |= QStyle::State_MouseOver;
+
+    EXPECT_CALL(*logsTableModelMock, data(index, ROLE_INSTRUMENT_LOGO))
+        .WillOnce(Return(QVariant(reinterpret_cast<qint64>(&logo))));
+    EXPECT_CALL(*logsTableModelMock, data(index, ROLE_INSTRUMENT_LOCKED)).WillOnce(Return(QVariant(true)));
+    EXPECT_CALL(*logsTableModelMock, data(index, ROLE_INSTRUMENT_NAME)).WillOnce(Return(QVariant("SPB Market")));
+
+    item->paint(&painter, option, index);
+
+    EXPECT_CALL(*logsTableModelMock, data(index, ROLE_INSTRUMENT_LOGO))
+        .WillOnce(Return(QVariant(reinterpret_cast<qint64>(&logo))));
+    EXPECT_CALL(*logsTableModelMock, data(index, ROLE_INSTRUMENT_LOCKED)).WillOnce(Return(QVariant(true)));
+    EXPECT_CALL(*logsTableModelMock, data(index, ROLE_INSTRUMENT_NAME)).WillOnce(Return(QVariant("")));
+
+    item->paint(&painter, option, index);
+
+    option.state &= ~QStyle::State_MouseOver;
+
+    EXPECT_CALL(*logsTableModelMock, data(index, ROLE_INSTRUMENT_LOGO))
+        .WillOnce(Return(QVariant(reinterpret_cast<qint64>(&logo))));
+    EXPECT_CALL(*logsTableModelMock, data(index, ROLE_INSTRUMENT_LOCKED)).WillOnce(Return(QVariant(true)));
+    EXPECT_CALL(*logsTableModelMock, data(index, Qt::DisplayRole)).WillOnce(Return(QVariant("SPBE")));
+
+    item->paint(&painter, option, index);
+}
+
+TEST_F(Test_InstrumentItemDelegate, Test_sizeHint)
+{
+    const InSequence seq;
+
+    EXPECT_CALL(*logsTableModelMock, rowCount(QModelIndex())).WillOnce(Return(1));
+    EXPECT_CALL(*logsTableModelMock, columnCount(QModelIndex())).WillOnce(Return(1));
+
+    QStyleOptionViewItem option;
+    QModelIndex          index = logsTableModelMock->index(0, 0);
+
+    EXPECT_CALL(*logsTableModelMock, data(index, ROLE_INSTRUMENT_LOCKED)).WillOnce(Return(QVariant(false)));
+    EXPECT_CALL(*logsTableModelMock, data(index, Qt::DisplayRole)).WillOnce(Return("SPBE"));
+
+    QSize size = item->sizeHint(option, index);
+
+    qInfo() << size; // TODO: Remove
+
+    ASSERT_EQ(size, QSize(57, 24));
+
+    EXPECT_CALL(*logsTableModelMock, data(index, ROLE_INSTRUMENT_LOCKED)).WillOnce(Return(QVariant(true)));
+    EXPECT_CALL(*logsTableModelMock, data(index, Qt::DisplayRole)).WillOnce(Return("SPBE"));
+
+    size = item->sizeHint(option, index);
+
+    qInfo() << size; // TODO: Remove
+
+    ASSERT_EQ(size, QSize(77, 24));
 }
