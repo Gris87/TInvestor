b8c580b0eec3126031debe35b6576c341902b79e
Gris87 <Gris87@yandex.ru>
2025-07-14 16:45:19 +0300

    Simple decision making

--- app/src/decisions/decisionmaker.cpp
+++ app/src/decisions/decisionmaker.cpp
@@ -24,5 +24,23 @@ DecisionMaker::makeDecision(const Portfolio& /*portfolio*/, const QList<Stock*>&
 
     // TODO: Implement
 
+    // TODO: Remove it [BEGIN]
+    const bool  needToBuy = QTime::currentTime().minute() % 2 == 0;
+    TradingInfo tradingInfo;
+
+    if (needToBuy)
+    {
+        tradingInfo.expectedCost = 1000.0;
+        tradingInfo.cause        = "I want to buy";
+    }
+    else
+    {
+        tradingInfo.expectedCost = 0.0;
+        tradingInfo.cause        = "I want to sell";
+    }
+
+    res["48bd9002-43be-4528-abf4-dc8135ad4550"] = tradingInfo;
+    // TODO: Remove it [END]
+
     return res;
 }
