c776d88b368776c8d1a40fad060ed5ccbb6705bf
Gris87 <Gris87@yandex.ru>
2025-08-10 10:48:17 +0300

    Fix style

--- app/src/decisions/buy/buydecision1/test_buydecision1.cpp
+++ app/src/decisions/buy/buydecision1/test_buydecision1.cpp
@@ -211,8 +211,10 @@ TEST_F(Test_BuyDecision1, Test_makeDecision)
 
     ASSERT_EQ(
         cause,
-        QString("Decided to buy because the price fall to 100.00 \u20BD from 120.00 \u20BD at %1 within last 3 minutes and the "
-                "fall is -16.67%")
+        QString(
+            "Decided to buy because the price fall to 100.00 \u20BD from 120.00 \u20BD at %1 within last 3 minutes and the "
+            "fall is -16.67%"
+        )
             .arg(QDateTime::fromMSecsSinceEpoch(stockOperationalData4.timestamp).toString(DATETIME_FORMAT))
     );
 
@@ -285,8 +287,10 @@ TEST_F(Test_BuyDecision1, Test_makeDecision)
 
     ASSERT_EQ(
         cause,
-        QString("Decided to buy because the price fall to 100.00 \u20BD from 120.00 \u20BD at %1 within last 3 minutes and the "
-                "fall is -16.67%")
+        QString(
+            "Decided to buy because the price fall to 100.00 \u20BD from 120.00 \u20BD at %1 within last 3 minutes and the "
+            "fall is -16.67%"
+        )
             .arg(QDateTime::fromMSecsSinceEpoch(stockData4.timestamp).toString(DATETIME_FORMAT))
     );
 }
