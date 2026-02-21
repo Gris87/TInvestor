163dfc2a38d126c5ac5db39a9ceee1388cbeceb4
Gris87 <Gris87@yandex.ru>
2026-02-22 00:15:19 +0300

    Ignore GTRK

--- app/src/threads/stockcollect/stockcollectthread.cpp
+++ app/src/threads/stockcollect/stockcollectthread.cpp
@@ -13,6 +13,7 @@
 const char* const RUBLE_UID                          = "a92e2e25-a698-45cc-a781-167cf465257c";
 const char* const LNZL_UID                           = "4563f7a1-8245-4caf-aba5-ac49827ba775";
 const char* const LNZLP_UID                          = "28fdec79-fcf0-40cb-b53c-586179f024e5";
+const char* const GTRK_UID                           = "9e69afb6-4561-4fc2-b63b-b181e3f9ecdc";
 constexpr qint64  MS_IN_SECOND                       = 1000LL;
 constexpr qint64  ONE_MINUTE                         = 60LL * MS_IN_SECOND;
 constexpr qint64  ONE_HOUR                           = 60LL * ONE_MINUTE;
@@ -27,9 +28,10 @@ constexpr int     HTTP_STATUS_CODE_OK                = 200;
 constexpr int     HTTP_STATUS_CODE_TOO_MANY_REQUESTS = 429;
 
 // clang-format off
-static const QMap<QString, bool> IGNORE_STOCKS{ // clazy:exclude=non-pod-global-static
-    {LNZL_UID,  true},
-    {LNZLP_UID, true}
+static const QSet<QString> IGNORE_STOCKS{ // clazy:exclude=non-pod-global-static
+    LNZL_UID,
+    LNZLP_UID,
+    GTRK_UID
 };
 // clang-format on
 
@@ -488,7 +490,7 @@ bool StockCollectThread::storeNewStocksInfo(const std::shared_ptr<tinkoff::Share
             stockMeta.lot                 = tinkoffStock.lot();
             stockMeta.pricePrecision      = quotationPrecision(tinkoffStock.min_price_increment());
             stockMeta.minPriceIncrement   = quotationToFloat(tinkoffStock.min_price_increment());
-            stockMeta.ignore              = IGNORE_STOCKS.value(stockMeta.instrumentId, false);
+            stockMeta.ignore              = IGNORE_STOCKS.contains(stockMeta.instrumentId);
 
             stocksMeta.append(stockMeta);
         }
