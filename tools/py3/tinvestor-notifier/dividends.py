b67936f28da84b8f35429da581f6251696b90440
Gris87 <Gris87@yandex.ru>
2026-06-15 12:27:51 +0300

    Improve notifier for dividends

--- tools/py3/tinvestor-notifier/dividends.py
+++ tools/py3/tinvestor-notifier/dividends.py
@@ -27,12 +27,34 @@ def check_dividends(args):
         with open(dividends_path, "r", encoding="utf-8") as f:
             old_dividends = json.loads(f.read())
 
-        for instrument_id, dividend in dividends.items():
-            if instrument_id not in old_dividends:
-                store_message(args, "dividends", msg_recommend_to_buy + "\n" + msg_dividends.format(ticker=dividend["ticker"], name=dividend["name"], yield_value=dividend["yieldValue"]))
+        new_dividends = {}
+        today = date.today()
 
-    with open(dividends_path, "w", encoding="utf-8") as f:
-        json.dump(dividends, f, ensure_ascii=False)
+        for date_str in list(old_dividends.keys()):
+            if date.fromisoformat(date_str) >= today:
+                new_dividends[date_str] = old_dividends[date_str]
+
+        for date_str, instruments in dividends.items():
+            new_instruments = {}
+
+            if date_str in new_dividends:
+                new_instruments = new_dividends[date_str]
+
+            for instrument_id, dividend in instruments.items():
+                if instrument_id not in new_instruments:
+                    new_instruments[instrument_id] = dividend
+
+                    store_message(args, "dividends", msg_recommend_to_buy + "\n" + msg_dividends.format(ticker=dividend["ticker"], name=dividend["name"], yield_value=dividend["yieldValue"], date_str=date_str))
+
+            new_dividends[date_str] = new_instruments
+
+        new_dividends = dict(sorted(new_dividends.items()))
+
+        with open(dividends_path, "w", encoding="utf-8") as f:
+            json.dump(new_dividends, f, ensure_ascii=False)
+    else:
+        with open(dividends_path, "w", encoding="utf-8") as f:
+            json.dump(dividends, f, ensure_ascii=False)
 
     return True
 
@@ -61,13 +83,16 @@ def _convert_calendar_to_dividends(calendar):
     res = {}
 
     for event in calendar:
-        date = event["date"]
+        date_str = event["date"]
         dividends = event["dividends"]
+
+        dividends_dict = {}
 
         for dividend in dividends:
             instrument_id = dividend["instrumentUid"]
-            dividend["date"] = date
 
-            res[instrument_id] = dividend
+            dividends_dict[instrument_id] = dividend
+
+        res[date_str] = dividends_dict
 
     return res
