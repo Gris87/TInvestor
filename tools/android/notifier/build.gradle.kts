cf8088d99c7d6bffc3e65127f9204e8e0a035efc
Gris87 <Gris87@yandex.ru>
2026-06-15 11:20:29 +0300

    Android coverage

--- tools/android/notifier/build.gradle.kts
+++ tools/android/notifier/build.gradle.kts
@@ -3,3 +3,14 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+
+android {
+    buildTypes {
+        debug {
+            // Enable coverage for unit tests
+            enableUnitTestCoverage = true
+            // Enable coverage for instrumentation tests
+            enableAndroidTestCoverage = true
+        }
+    }
+}
