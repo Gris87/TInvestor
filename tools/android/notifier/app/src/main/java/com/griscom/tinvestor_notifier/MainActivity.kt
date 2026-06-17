dee52e16c24d82eaf204ec64280b5ecf64144459
Gris87 <Gris87@yandex.ru>
2026-06-17 15:04:54 +0300

    Android Notifier

--- tools/android/notifier/app/src/main/java/com/griscom/tinvestor_notifier/MainActivity.kt
+++ tools/android/notifier/app/src/main/java/com/griscom/tinvestor_notifier/MainActivity.kt
@@ -4,44 +4,93 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.griscom.tinvestor_notifier.ui.theme.TInvestorNotifierTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             TInvestorNotifierTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ConversationContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ConversationContent() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { TopBar() }
+    ) { innerPadding ->
+        ScrollContent(innerPadding)
+    }
+}
+
+@Composable
+fun TopBar() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    TopAppBar(
+        title = { Text(stringResource(R.string.app_name)) },
+        actions={
+            Icon(
+                painterResource(R.drawable.ic_search),
+                tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .height(24.dp),
+                contentDescription = stringResource(R.string.content_description_search),
+            )
+        }
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ScrollContent(innerPadding: PaddingValues) {
+    Column(Modifier.padding(innerPadding)) {
+        Text("fdfh")
+    }
+}
+
+@Preview
+@Composable
+fun ConversationContentPreview() {
     TInvestorNotifierTheme {
-        Greeting("Android")
+        ConversationContent()
+    }
+}
+
+@Preview
+@Composable
+fun TopBarPreview() {
+    TInvestorNotifierTheme {
+        TopBar()
+    }
+}
+
+@Preview
+@Composable
+fun ScrollContentPreview() {
+    TInvestorNotifierTheme {
+        ScrollContent(PaddingValues())
     }
 }
