f9ab42631a6d1ccae07c3db00a33a84035003fc7
Gris87 <Gris87@yandex.ru>
2026-07-16 12:05:22 +0300

    Fix tests

--- tools/android/notifier/app/src/test/java/com/griscom/tinvestor_notifier/activities/SettingsActivityUnitTest.kt
+++ tools/android/notifier/app/src/test/java/com/griscom/tinvestor_notifier/activities/SettingsActivityUnitTest.kt
@@ -18,6 +18,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.GraphicsMode
+import org.robolectric.shadows.ShadowLooper
 
 @RunWith(RobolectricTestRunner::class)
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
@@ -38,7 +39,7 @@ class SettingsActivityUnitTest {
                                     changeThreshold = 0.01F,
                                     imageComparator =
                                         SimpleImageComparator(
-                                            maxDistance = 0.07F,
+                                            maxDistance = 0.007F,
                                             vShift = 1,
                                             hShift = 1,
                                         ),
@@ -64,71 +65,105 @@ class SettingsActivityUnitTest {
     @Test
     fun update_preferences() =
         runTest {
+            composeTestRule.mainClock.autoAdvance = false
+
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/01_start.png")
 
             composeTestRule.onNodeWithTag("server_address_field").performTextReplacement("yandex.ru")
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/02_text_fields_updated_01.png")
 
             composeTestRule.onNodeWithTag("server_address_field").performTextReplacement("localhost")
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/03_text_fields_updated_02.png")
 
             composeTestRule.onNodeWithTag("server_port_field").performTextReplacement("8888")
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/04_text_fields_updated_03.png")
 
             composeTestRule.onNodeWithTag("server_port_field").performTextReplacement("8041")
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/05_text_fields_updated_04.png")
 
             composeTestRule.onNodeWithTag("show_notifications_switch").performClick()
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/06_show_notifications_toggled_01.png")
 
             composeTestRule.onNodeWithTag("show_notifications_switch").performClick()
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/07_show_notifications_toggled_02.png")
 
             composeTestRule.onNodeWithTag("filter_select_all_checkbox").performClick()
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/08_filter_select_all_toggled_01.png")
 
             composeTestRule.onNodeWithTag("filter_select_all_checkbox").performClick()
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/09_filter_select_all_toggled_02.png")
 
             composeTestRule.onNodeWithTag("filter_system_checkbox").performClick()
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/10_filter_toggled_01.png")
 
             composeTestRule.onNodeWithTag("filter_portfolio_checkbox").performClick()
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/11_filter_toggled_02.png")
 
             composeTestRule.onNodeWithTag("filter_huge_sell_checkbox").performClick()
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/12_filter_toggled_03.png")
 
             composeTestRule.onNodeWithTag("filter_dividends_checkbox").performClick()
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/13_filter_toggled_04.png")
 
             composeTestRule.onNodeWithTag("filter_pulse_neutral_checkbox").performClick()
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/14_filter_toggled_05.png")
 
             composeTestRule.onNodeWithTag("filter_pulse_buy_checkbox").performClick()
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/15_filter_toggled_06.png")
 
             composeTestRule.onNodeWithTag("filter_pulse_sell_checkbox").performClick()
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/16_filter_toggled_07.png")
 
             composeTestRule.onNodeWithTag("filter_pulse_neutral_checkbox").performClick()
             composeTestRule.onNodeWithTag("filter_pulse_buy_checkbox").performClick()
             composeTestRule.onNodeWithTag("filter_pulse_sell_checkbox").performClick()
+            composeTestRule.mainClock.advanceTimeBy(10000)
+            ShadowLooper.idleMainLooper()
 
             composeTestRule.onRoot().captureRoboImage("SettingsActivityUnitTest/update_preferences/17_finish.png")
         }
