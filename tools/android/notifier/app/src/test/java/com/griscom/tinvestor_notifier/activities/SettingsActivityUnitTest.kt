68f9ddc371921369602447efc442baba937a0a67
Gris87 <Gris87@yandex.ru>
2026-07-16 11:34:29 +0300

    Fix tests

--- tools/android/notifier/app/src/test/java/com/griscom/tinvestor_notifier/activities/SettingsActivityUnitTest.kt
+++ tools/android/notifier/app/src/test/java/com/griscom/tinvestor_notifier/activities/SettingsActivityUnitTest.kt
@@ -38,7 +38,7 @@ class SettingsActivityUnitTest {
                                     changeThreshold = 0.01F,
                                     imageComparator =
                                         SimpleImageComparator(
-                                            maxDistance = 0.06F,
+                                            maxDistance = 0.07F,
                                             vShift = 1,
                                             hShift = 1,
                                         ),
